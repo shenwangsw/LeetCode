a113a6b9a65fc1420d259d5143a5c88ae513857c
shenwang <844786092@qq.com>
2022-07-21 10:01:18 +0800

    fitst

--- src/javaBase/Base01.java
+++ src/javaBase/Base01.java
@@ -2,20 +2,6 @@ package javaBase;
 
 public class Base01 {
     public static void main(String[] args) {
-        int a = 5;
-        long b = a;
-        double d= b;
 
-        int money = 1000000000;
-        int years = 20;
-        int total = money*years;
-
-        System.out.println("total"+total);
-
-        long totals = money*years;
-        System.out.println(totals);
-
-        long total2 = 1L*money*years;
-        System.out.println(total2);
     }
 }
